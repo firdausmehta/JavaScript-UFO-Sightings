dee8eea18ae14528266194d237a13911c871ecc8
Firdaus Mehta <firmehta97@gmail.com>
2021-08-23 18:28:53 +1000

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,58 +3,42 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime");
-var inputField2 = d3.select("#city");
-var resetbtn = d3.select("#reset-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-var populate = (dataInput) => {
+// Function build table
+function buildTable(data){
+    // First, clear out existing data
+    tbody.html("");
+    data.forEach(dataRow => {
+        console.table(dataRow);
+        let row = tbody.append("tr");
 
-	dataInput.forEach(ufo_sightings => {
-		var row = tbody.append("tr");
-		columns.forEach(column => row.append("td").text(ufo_sightings[column])
-		)
-	});
+       console.table(Object.values(dataRow));
+       Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
+    });
 }
 
-//Populate table
-populate(data);
-
-// Filter by attribute
-button.on("click", () => {
-	d3.event.preventDefault();
-	var inputDate = inputField1.property("value").trim();
-	var inputCity = inputField2.property("value").toLowerCase().trim();
-	// Filter by field matching input value
-	var filterDate = data.filter(data => data.datetime === inputDate);
-	console.log(filterDate)
-	var filterCity = data.filter(data => data.city === inputCity);
-	console.log(filterCity)
-	var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
-	console.log(filterData)
+// Event that calls a function  name it handleClick
+function handleClick(){
+    d3.event.preventDefault() // prevent the form from refreshing the page
     
-	// Add filtered sighting to table
-	tbody.html("");
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
 
-	let response = {
-		filterData, filterCity, filterDate
-	}
+    // Check to see if a date was entered and filter the data using that date
+    if (date){
+        // Apply filter to the table data to only keep the 
+        // rows where the datetime value matches the filter value
+        filterData = filterData.filter((row) => row.datetime === date);
 
-	if (response.filterData.length !== 0) {
-		populate(filterData);
-	}
-		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-			populate(filterCity) || populate(filterDate);
-	
-		}
-		else {
-			tbody.append("tr").append("td").text("No results found!"); 
-		}
-})
+    }
 
-resetbtn.on("click", () => {
-	tbody.html("");
-	populate(data)
-	console.log("Table reset")
-})
+    buildTable(filterData);
+}
+
+
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
